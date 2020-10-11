d4b1d3618e326c29203462e5edaa4d6214ad2c21
medit <66057954+meditq@users.noreply.github.com>
2020-10-12 05:07:58 +0900

    解答時間を変数で設定できるようにした

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #include <fcntl.h>
 
 int main(int argc, char **argv){
-	int life = 1, score = 1, i, j, barsnow, barsold, ans, input, cont;
+	int life = 1, score = 1, anstime = 1;
+	int i, j, barsnow, barsold, ans, input, cont;
 	char c;
 	struct timespec ts;
 	struct timeval tstart, tnow;
@@ -26,21 +27,22 @@ If not specified, 1 is used.\n\
 	}
 	system("stty -echo -icanon");
 	fcntl(0, F_SETFL, fcntl(0, F_GETFL, 0) | O_NONBLOCK);
-	printf("*** FizzBuzz Game ***\n\
+	printf("\
+*** FizzBuzz Game ***\n\
 \n\
 [How to play]\n\
 Press [Left]  key to say \"Fizz\".\n\
 Press [Right] key to say \"Buzz\".\n\
 Press [Down]  key to say the displayed number.\n\
 \n\
-You must answer in 1 sec.\n\
+You must answer in %d sec.\n\
 You lose one life when you make a mistake or don't answer in time.\n\
 \n\
-");
+", anstime);
 	printf("Life:");
 	for(int i = 0; i < life; i++) printf(" *");
 	printf("\n\n              ->  \n\n");
-	printf("( 1000 ms )[");
+	printf("( %4d ms )[", anstime * 1000);
 	for(i = 0; i < 100; i++) putchar('|');
 	printf("]\n* Press [Space] key to start *\n");
 	while(getchar() != ' ');
@@ -63,7 +65,7 @@ You lose one life when you make a mistake or don't answer in time.\n\
 		cont = 0;
 		while(1){
 			gettimeofday(&tnow, NULL);
-			remainus = 1000000L;
+			remainus = anstime * 1000000L;
 			remainus -= (tnow.tv_sec - tstart.tv_sec) * 1000000L + (tnow.tv_usec - tstart.tv_usec);
 			if(remainus < 0) remainus = 0;
 			while((c = getchar()) != EOF){
@@ -92,7 +94,7 @@ You lose one life when you make a mistake or don't answer in time.\n\
 				}
 			}
 			barsold = barsnow;
-			barsnow = (remainus + 9999) / 10000;
+			barsnow = (remainus + anstime * 10000 - 1) / (anstime * 10000);
 			printf("\e[2F\e[2C%4ld\e[%dC", (remainus + 999) / 1000, 6 + barsnow);
 			for(i = barsnow; i < barsold; i++) putchar(' ');
 			printf("\n\n");
